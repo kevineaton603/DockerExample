9b4ca9658ccf5f4c6a91aeb10bb45025675533dc
kevineaton603 <kevin.eaton@mymail.champlain.edu>
2020-06-29 02:44:05 -0400

    docker compose example with db

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const port = 8080;
+
+mongoose
+    .connect(`mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@database:27017`, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log("Database Connected"))
+    .catch((err) => console.log(`Database Connection Failed: ${err}`));
 
 const app = express();
 
